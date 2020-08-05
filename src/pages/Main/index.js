16ca777a972b71f9ba73ea05e713360d05c78692
DanGM96 <jordangm@outlook.com>
2020-08-05 15:25:31 -0300

    Criando a Main page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,22 +1,63 @@
-import React, { useState } from 'react';
-import { useFocusEffect } from '@react-navigation/native';
-import { View, Text, Button } from 'react-native';
-import { Container, SearchBar } from './styles';
+import React, { useState } from "react";
+import { useFocusEffect } from "@react-navigation/native";
+
+import {
+	Container,
+	SearchBar,
+	SubContainer,
+	ListSubContainer,
+	TextWhite,
+	ShowItem,
+} from "./styles";
+
+import api from "../../services/api";
 
 export default function index({ navigation }) {
-	const [searchTitle, setSearchTitle] = useState('');
+	const [searchText, setSearchText] = useState("");
+	const [showList, setShowList] = useState([]);
+
+	async function loadSearchResults() {
+		const url = `shows?q=${searchText}`;
+		const response = await api.get(url);
+		const showData = response.data;
+		setShowList(showData);
+	}
+
+	useFocusEffect(
+		React.useCallback(() => {
+			loadSearchResults();
+		}, [searchText])
+	);
 
 	return (
-		<Container source={require('../../../assets/main.jpg')}>
-			<SearchBar
-				placeholder={'Show title...'}
-				onChangeText={(text) => setSearchTitle(text)}
-				defaultValue={searchTitle}
-			/>
-			<Button
-				title="More info"
-				onPress={() => navigation.navigate('Info', { id: 3 })}
-			/>
+		<Container source={require("../../../assets/main.jpg")}>
+			<SubContainer>
+				<SearchBar
+					placeholder={"Search title..."}
+					onChangeText={(text) => setSearchText(text)}
+					defaultValue={searchText}
+				/>
+
+				<ListSubContainer>
+					{showList.length > 0 &&
+						showList.map((item) => {
+							return (
+								<ShowItem
+									onPress={() => navigation.navigate("Info", { id: item.id })}
+									key={item.id}
+								>
+									{/* <Image
+										source={{
+											uri: item.image.medium,
+										}}
+										style={{ width: '3%', height: '3%' }}
+									/> */}
+									<TextWhite>{item.name}</TextWhite>
+								</ShowItem>
+							);
+						})}
+				</ListSubContainer>
+			</SubContainer>
 		</Container>
 	);
 }
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,17 +1,45 @@
-import { StatusBar } from 'react-native';
-import styled from 'styled-components';
+import { StatusBar } from "react-native";
+import styled from "styled-components";
 const STATUSBAR_HEIGHT = StatusBar.currentHeight;
 
 export const Container = styled.ImageBackground`
 	flex: 1;
-	padding-top: ${STATUSBAR_HEIGHT}px;
 `;
 
 export const SearchBar = styled.TextInput`
-	margin: 30px;
+	margin: 20px;
 	height: 50px;
 	padding-left: 15px;
 	background-color: rgba(27, 27, 27, 0.9);
 	color: white;
 	font-size: 20px;
+	border-radius: 6px;
+`;
+
+export const SubContainer = styled.View`
+	flex: 1;
+	background-color: rgba(27, 27, 27, 0.6);
+`;
+
+export const TextWhite = styled.Text`
+	color: #fff;
+	font-size: 17px;
+	margin-top: 7px;
+`;
+
+export const ListSubContainer = styled.ScrollView`
+	margin: 15px;
+	margin-top: 0px;
+	margin-bottom: 30px;
+	padding: 0px;
+	background-color: rgba(27, 27, 27, 0.8);
+	border-radius: 6px;
+`;
+
+export const ShowItem = styled.TouchableOpacity`
+	margin: 5px;
+	border: 1px solid white;
+	align-items: center;
+	justify-content: center;
+	border-radius: 6px;
 `;
